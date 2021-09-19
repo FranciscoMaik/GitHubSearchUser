db37dc65b05b0b87afb5146fd20ea0d07751db2e
Francisco Maik <chiconunes3081@gmail.com>
2021-09-19 20:22:13 -0300

    feat: dados dinamicos na pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,9 @@
-import Card from '../components/Card';
+import { useState, useCallback } from 'react'; 
+import { AiOutlineSearch } from 'react-icons/ai';
+
+import api from '../services/Api';
+
+import { Card } from '../components/Card';
 
 import { 
   Container,
@@ -7,27 +12,67 @@ import {
   DivHeadAlign, 
   Input, 
   ButtonSearch, 
-  Body 
+  Body
 } from '../styles/pages/Home'
 
+interface IRepos {
+  id: number;
+  name: string;
+  description: string;
+  stargazers_count: number;
+  language: string;
+  forks: number;
+  clone_url: string;
+}
 
 export default function Home() {
+  const [repos, setRepos] = useState<IRepos[]>([])
+  const [repo, setRepo] = useState('');
+  const [inputValue, setInputValue] = useState<boolean>(false);
+
+  const getRepos = useCallback(async()=> {
+    const response = await api.get(`${repo}/repos`);
+    if(response.data.id !== 77416){
+      setRepos(response.data);
+      setRepo('');
+      setInputValue(false);
+    }else{
+      alert('User not found!');
+      setInputValue(true);
+    }
+  },[repo])
+  
+
   return (
     <Container>
       <Head>
         <Title>GitHub Search User</Title>
         <DivHeadAlign>          
-          <Input placeholder="User: ex.: jhondoe "/>
-          <ButtonSearch>Pesquisar</ButtonSearch>
+          <Input
+            inputValue={inputValue} 
+            placeholder="User: ex.: jhondoe " 
+            value={repo} 
+            onChange={(e) => setRepo(e.target.value)}
+          />
+          <ButtonSearch onClick={getRepos}><AiOutlineSearch /></ButtonSearch>
         </DivHeadAlign>  
       </Head>
+
       <Body>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {repos.map((item) => {
+          return(
+            <Card 
+              key={item.id} 
+              description={item.description} 
+              forks={item.forks}
+              language={item.language}
+              name={item.name}
+              stargazers_count={item.stargazers_count}
+              clone_url={item.clone_url}
+            />
+          )
+        })}
+        
       </Body>
     </Container>
   )
