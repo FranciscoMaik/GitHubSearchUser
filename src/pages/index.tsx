079233ba0261040331fdc491b0ae598dd748fcdc
Francisco Maik <chiconunes3081@gmail.com>
2021-09-19 17:23:11 -0300

    feat: organizando os dados da pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,13 +15,18 @@ export default function Home() {
   return (
     <Container>
       <Head>
-        <Title>GitHub Search Usuário</Title>
+        <Title>GitHub Search User</Title>
         <DivHeadAlign>          
-          <Input placeholder="Repositório: ex.: jhondoe/java "/>
+          <Input placeholder="User: ex.: jhondoe "/>
           <ButtonSearch>Pesquisar</ButtonSearch>
         </DivHeadAlign>  
       </Head>
       <Body>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
         <Card />
       </Body>
     </Container>
